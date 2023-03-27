40bc36ab72efcdcc394f022952097752c8723125
oleg <legik7127@gmail.com>
2023-03-27 20:43:20 +0600

    have worked with different exceptions

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,20 @@
 import java.io.File;
-import java.util.Objects;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
 //        String name = null;
 //        System.out.println(name.length());
-        Object object = new String("123");
-        File file = (File) object;
-        System.out.println(file);
+
+//        Object object = new String("123");
+//        File file = (File) object;
+//        System.out.println(file);
+
+//        String number = "123fq";
+//        int result =Integer.parseInt(number);
+//        System.out.println(result);
+
+//        List<Object> emptylist = Collections.emptyList();
+//        emptylist.add(new Object());
     }
 }
