ac93e5d1c66c2cfbc7818fd7c21d3b5a168005d0
oleg <legik7127@gmail.com>
2023-03-27 21:48:43 +0600

    added block finally

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,7 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Application {
@@ -14,19 +17,47 @@ public class Application {
 //        int result =Integer.parseInt(number);
 //        System.out.println(result);
 
-//        List<Object> emptylist = Collections.emptyList();
+        //       List<Object> emptylist = Collections.emptyList();
 //        emptylist.add(new Object());
 
-        int number = 1;
+//        int number = 1;
+//        try {
+//            number = 10 / 1;
+//            String test = null;
+////            System.out.println(test.length());
+//            Collections.emptyList().add(new Object());
+//        } catch (ArithmeticException e) {
+//            System.out.println("operation divide by zero not supported");
+//        } catch (NullPointerException e) {
+//            System.out.println("nullPointer exception");
+//        } catch (Exception e){
+//            System.out.println("exception");
+//        }
+//        System.out.println(number);
+
+//        try {
+//            FileReader test = new FileReader("test");
+//        } catch (FileNotFoundException | RuntimeException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        FileReader test = null;
         try {
-            number = 10/1;
-            String test = null;
-            System.out.println(test.length());
-        } catch (ArithmeticException e) {
-            System.out.println("operation divide by zero not supported");
-        } catch (NullPointerException e){
-            System.out.println("nullpointer exception");
+            test = new FileReader("/Users/olegmonogarov/IdeaProjects/Exceptions_lecture_2/gradlew");
+            test.read();
+        } catch (RuntimeException | IOException e) {
+            System.out.println("catch exception: " + e.getClass().getSimpleName());
         }
-        System.out.println(number);
+        finally {
+            System.out.println("finally start");
+            if (test != null){
+                try {
+                    test.close();
+                } catch (IOException e) {
+                    System.out.println("exception while close");;
+                }
+            }
+            System.out.println("finally finished");
+        }
     }
 }
